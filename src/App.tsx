d0288834dda3bccee78d3e0486b7d84c29c89d4f
Jenni Foster <jennisimone@gmail.com>
2025-09-08 18:51:32 +0100

    amend homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,66 +4,59 @@ import './App.css'
 
 // Import page components
 import Home from './pages/Home'
-import AboutUs from './pages/AboutUs'
-import Events from './pages/Events.tsx'
 import Gallery from './pages/Gallery'
 
 function App() {
-  const [isNavOpen, setIsNavOpen] = useState(false)
+    const [isNavOpen, setIsNavOpen] = useState(false)
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen)
-  }
-
-  // Close mobile menu when a link is clicked
-  const closeMenu = () => {
-    if (isNavOpen) {
-      setIsNavOpen(false)
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen)
     }
-  }
 
-  return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <div className="navbar-logo">
-            <img className={"logo"} src={"QIT-logo.jpg"} alt={"logo"}/>
-            <Link to="/" onClick={closeMenu}>Queer in Tech</Link>
-          </div>
+    // Close mobile menu when a link is clicked
+    const closeMenu = () => {
+        if (isNavOpen) {
+            setIsNavOpen(false)
+        }
+    }
 
-          <div className={`navbar-toggle ${isNavOpen ? 'active' : ''}`} onClick={toggleNav}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <div className="navbar-logo">
+                        <img className={"logo"} src={"QIT-logo-1.jpg"} alt={"logo"}/>
+                        <Link to="/" onClick={closeMenu}>Queer in Tech</Link>
+                    </div>
 
-          <ul className={`navbar-menu ${isNavOpen ? 'active' : ''}`}>
-            <li className="navbar-item">
-              <Link to="/" className="navbar-link" onClick={closeMenu}>Home</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/about-us" className="navbar-link" onClick={closeMenu}>About Us</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/events" className="navbar-link" onClick={closeMenu}>Events</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/gallery" className="navbar-link" onClick={closeMenu}>Gallery</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+                    <div className={`navbar-toggle ${isNavOpen ? 'active' : ''}`} onClick={toggleNav}>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </div>
 
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-      </div>
-    </>
-  )
+                    <ul className={`navbar-menu ${isNavOpen ? 'active' : ''}`}>
+                        <li className="navbar-item">
+                            <Link to="/" className="navbar-link" onClick={closeMenu}>Home</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="https://donate.stripe.com/00waEQ4AL0tHgxU72W0x200">Donate</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/gallery" className="navbar-link" onClick={closeMenu}>Gallery</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/gallery" element={<Gallery/>}/>
+                </Routes>
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import './Home.scss';
 
 const Home: React.FC = () => {
   return (
     <div className="home-page">
-      <h1>Welcome to Queer in Tech</h1>
+            <h2 className={"title"}>Queer in Tech</h2>
+            <div>Queer in tech has been operating since 2023, we run quarterly events which allow for LGBTQ+ people and
+                allies to attend, learn from and learn about other queer people in tech.
+            </div>
+        <div className="find-us">
+            <div>
+                <h3>You can find our events over on meetup</h3>
+                <a href={"https://www.meetup.com/queer-in-tech-inclusive-tech-meetup"} target={"_blank"}><img className={"icon"}
+                                                                                                              src={"./meetup.jpeg"}
+                                                                                                              alt={"meetup link"}/></a>
+            </div>
+            <div>
+                <h3>Follow our linkedin page to get regular updates</h3>
+                <a href={"https://www.linkedin.com/company/queer-in-tech-uk"} target={"_blank"}><img className={"icon"}
+                                                                                                     src={"./linkedin.jpg"}
+                                                                                                     alt={"meetup link"}/></a>
+            </div>
+        </div>
+            <h2>Meet the team</h2>
+            <div className={"person-container"}>
+                <PersonCard name={"Dmitry"} image={"./dmitry.jpeg"}/>
+                <PersonCard name={"Stevie"} image={"./stevie.jpeg"}/>
+                <PersonCard name={"Jenni"} image={"./jenni.jpeg"}/>
+            </div>
+            <div className={"person-container"}>
+                <PersonCard name={"Rebecca"} image={"./rebecca.jpeg"}/>
+                <PersonCard name={"Joe"} image={"./joe.jpeg"}/>
+                <PersonCard name={"Ari"} image={"./ari.jpeg"}/>
+            </div>
     </div>
   );
 };
+
+const PersonCard = (props: Person) => {
+    return (
+        <div className={"person-card"}>
+            <img src={props.image} alt={props.name}/>
+            {props.name}
+        </div>
+    )
+}
+
+interface Person {
+    name: string,
+    image: string,
+}
 
 export default Home;

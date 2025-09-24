735552a2d20aec188da6b5e31e02239b953ea6fd
Dmitry Leyko <dmitryleyko@Dmitrys-MacBook-Air.local>
2025-09-24 15:26:41 +0100

    remove actions from contacts page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -85,53 +85,6 @@ function ActionButtons() {
   );
 }
 
-function DonateIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-tip-jar-pound"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M17 4v1.882c0 .685 .387 1.312 1 1.618s1 .933 1 1.618v8.882a3 3 0 0 1 -3 3h-8a3 3 0 0 1 -3 -3v-8.882c0 -.685 .387 -1.312 1 -1.618s1 -.933 1 -1.618v-1.882" />
-      <path d="M6 4h12z" />
-      <path d="M14 10h-1a2 2 0 0 0 -2 2v2c0 1.105 -.395 2 -1.5 2h4.5" />
-      <path d="M10 13h3" />
-    </svg>
-  );
-}
-
-function NextEventIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
-      <path d="M16 3l0 4" />
-      <path d="M8 3l0 4" />
-      <path d="M4 11l16 0" />
-      <path d="M8 15h2v2h-2z" />
-    </svg>
-  );
-}
-
 function IdeaIcon() {
   return (
     <svg
